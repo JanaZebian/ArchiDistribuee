67a9c5d0dfd7809a7c2a7f8e7fc0ef384ae54b50
Jana Zebian <jana.zebian@etu.u-bordeaux.fr>
2023-10-14 12:21:59 +0200

    TP1 waiting for booking's code

---
+++ UE-AD-A1-REST/showtime/showtime.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify, make_response
+import json
+from werkzeug.exceptions import NotFound
+
+app = Flask(__name__)
+
+PORT = 3202
+HOST = '0.0.0.0'
+
+with open('{}/databases/times.json'.format("."), "r") as jsf:
+   schedule = json.load(jsf)["schedule"]
+
+@app.route("/", methods=['GET'])
+def home():
+   return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+
+@app.route("/showtimes", methods=['GET'])
+def get_schedule():
+   res= make_response(jsonify(schedule), 200)
+   return res
+
+@app.route("/showmovies/<date>", methods=['GET'])
+def get_movies_bydate(date):
+   for time in schedule:
+      if str(time["date"]) == str(date):
+            res = make_response(jsonify(time),200)
+            return res
+   return make_response(jsonify({"error":"No movies with this date has been found"}),400)
+
+if __name__ == "__main__":
+   print("Server running in port %s"%(PORT))
+   app.run(host=HOST, port=PORT, debug=True)
---
+++ UE-AD-A1-REST/user/user.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, jsonify, make_response
+import requests
+import json
+from werkzeug.exceptions import NotFound
+
+app = Flask(__name__)
+
+PORT = 3203
+HOST = '0.0.0.0'
+
+with open('{}/databases/users.json'.format("."), "r") as jsf:
+   users = json.load(jsf)["users"]
+
+@app.route("/", methods=['GET'])
+def home():
+   return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+
+@app.route("/booking/{userid}", methods=['GET'])
+def get_booking_by_userid(userid):
+      for user in users:
+         if str(user["id"]) == str(userid):
+            data = {"userid" : userid} #create
+            r = requests.get("http://localhost:3201/booking", data = data) #send the request and get a response
+            rj= r.json() # get the json from the response
+            # rj["t"] acceder au champs du json t has to be just like kif huwe 7atu 3ndu l reponse 3a format ligne 23
+            res = make_response(jsonify(rj), 200)
+            return res
+      return make_response(jsonify({"error": "Date not available"}),400)
+         
+
+if __name__ == "__main__":
+   print("Server running in port %s"%(PORT))
+   app.run(host=HOST, port=PORT)
