c037c17c7936f2d1898cada54aef07ba5081f130
Jana Zebian <jana.zebian@etu.u-bordeaux.fr>
2023-10-14 16:27:19 +0200

    no major changes

--- UE-AD-A1-REST/user/user.py
+++ UE-AD-A1-REST/user/user.py
@@ -9,25 +9,29 @@ PORT = 3203
 HOST = '0.0.0.0'
 
 with open('{}/databases/users.json'.format("."), "r") as jsf:
-   users = json.load(jsf)["users"]
+    users = json.load(jsf)["users"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+
 
 @app.route("/booking/<userid>", methods=['GET'])
 def get_booking_by_userid(userid):
-      for user in users:
-         if str(user["id"]) == str(userid):
-            data = {"userid" : userid} #create
-            r = requests.get("http://localhost:3201/booking", data = data) #send the request and get a response
-            rj= r.json() # get the json from the response
+    for user in users:
+        if str(user["id"]) == str(userid):
+            data = {"userid": userid}  # create
+            r = requests.get("http://localhost:3201/booking", data=data)  # send the request and get a response
+            rj = r.json()  # get the json from the response
             # rj["t"] acceder au champs du json t has to be just like kif huwe 7atu 3ndu l reponse 3a format ligne 23
             res = make_response(jsonify(rj), 200)
             return res
-      return make_response(jsonify({"error": "Date not available"}),400)
-         
+    return make_response(jsonify({"error": "Date not available"}), 400)
+
+
+# do another entry point
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
