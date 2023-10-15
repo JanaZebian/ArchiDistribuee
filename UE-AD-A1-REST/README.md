4be9fb174dd98bf4c3fc0ecdf685a8d86a1c5202
Jana Zebian <jana.zebian@etu.u-bordeaux.fr>
2023-10-15 14:37:25 +0200

    commented movie.py, corrected something in the yaml and started with the Readme

--- UE-AD-A1-REST/README.md
+++ UE-AD-A1-REST/README.md
@@ -1,3 +1,5 @@
 # UE-AD-A1-REST
 
-test
+! needs to be finished !
+
+#### Dans ce TP on a 4 services/serveurs: user, movie, booking et time qui se communiquent en utilisant l'API REST, pour que le client puisse envoyer des requetes pour demander au services des informations sur un des services ou plusieurs. 
--- UE-AD-A1-REST/movie/movie.py
+++ UE-AD-A1-REST/movie/movie.py
@@ -8,86 +8,107 @@ app = Flask(__name__)
 PORT = 3200
 HOST = '0.0.0.0'
 
+# reading the json file corresponding to the movie service
 with open('{}/databases/movies.json'.format("."), "r") as jsf:
-   movies = json.load(jsf)["movies"]
+    movies = json.load(jsf)["movies"]
 
-# root message
-@app.route("/",methods=["GET"]) #par defaut
-def listofmovies(): # Listez tous les films existants dans la base de donnée 
+
+# root message/ default that returns the list of movies in the database
+@app.route("/", methods=["GET"])
+def listofmovies():
     res = ""
     for movie in movies:
         res += movie["title"] + "<br>"
-    return res 
-
-#def home():
-    return make_response("<h1 style='color:blue'>Welcome all to the Movie service !</h1>",200)
-
-@app.route("/template", methods=['GET']) #point d'entree
-def template():
-    return make_response(render_template('index.html', body_text='This is my HTML template for Movie service'),200)
-
-@app.route("/json", methods=['GET'])
-def get_json():
-    res = make_response(jsonify(movies), 200)
     return res
 
+
+# first entry point: requesting through a get method a template
+@app.route("/template", methods=['GET'])
+def template():
+    return make_response(render_template('index.html', body_text='This is my HTML template for Movie service'), 200)
+
+
+# second entry point: requesting through a get method the movies' json database
+@app.route("/json", methods=['GET'])
+def get_json():
+    return make_response(jsonify(movies), 200)
+
+
+# third entry point: requesting through a get method the information on a movie by his ID
 @app.route("/movies/<movieid>", methods=['GET'])
 def get_movie_byid(movieid):
     for movie in movies:
+        # making sure that the movie ID exists in the database before answering the request
         if str(movie["id"]) == str(movieid):
-            res = make_response(jsonify(movie),200)
-            return res
-    return make_response(jsonify({"error":"Movie ID not found"}),400)
+            return make_response(jsonify(movie), 200)
+    return make_response(jsonify({"error": "Movie ID not found"}), 400)
 
-@app.route("/moviesbytitle", methods=['GET'])
-def get_movie_bytitle():
+
+# fourth entry point: requesting through a get accessor the information on a movie by passing its title in the path in a
+# json format
+@app.route("/moviebytitle", methods=['GET'])
+def get_movie_by_title():
     json = ""
+    # making sure that the passed parameter is valid
     if request.args:
         req = request.args
         for movie in movies:
+            # seeing if the title passed in the path exists in the movies database
             if str(movie["title"]) == str(req["title"]):
+                # selecting the movie
                 json = movie
-
+# checking if the movie title exists and if not return an error message with the corresponding status code 400 or 200
     if not json:
-        res = make_response(jsonify({"error":"movie title not found"}),400)
+        res = make_response(jsonify({"error": "movie title not found"}), 400)
     else:
-        res = make_response(jsonify(json),200)
+        res = make_response(jsonify(json), 200)
     return res
 
+
+# fifth entry point: through a post accessor requesting to add/create a movie by passing its ID
 @app.route("/movies/<movieid>", methods=['POST'])
 def create_movie(movieid):
+    # accessing the json database with the request method from Flask
     req = request.get_json()
 
+    # checking whether the movie id exists already because if it does there's no need to have the same movie twice
     for movie in movies:
         if str(movie["id"]) == str(movieid):
-            return make_response(jsonify({"error":"movie ID already exists"}),409)
-
+            return make_response(jsonify({"error": "movie ID already exists"}), 409)
+    # adding the movie to the database
     movies.append(req)
-    res = make_response(jsonify({"message":"movie added"}),200)
-    return res
+    return make_response(jsonify({"message": "movie added"}), 200)
 
+
+# sixth entry point: through a PUT accessor modify the rating of a movie by passing its ID and new rating as argument in
+# the path
 @app.route("/movies/<movieid>/<rate>", methods=['PUT'])
 def update_movie_rating(movieid, rate):
     for movie in movies:
+        # like before checking if the movieID exists is necessary
         if str(movie["id"]) == str(movieid):
+            # after the id is found, modify that movie's rating by giving it the rating passed as an argument
             movie["rating"] = int(rate)
-            res = make_response(jsonify(movie),200)
-            return res
+            # status code 201 is to indicate that the request has succeeded and has led to the creation of a resource
+            return make_response(jsonify(movie), 201)
 
-    res = make_response(jsonify({"error":"movie ID not found"}),201)
-    return res
+    return make_response(jsonify({"error": "movie ID not found"}), 400)
 
+
+# seventh entry point: through the delete method, remove a movie also by passing its ID as an argument
 @app.route("/movies/<movieid>", methods=['DELETE'])
 def del_movie(movieid):
     for movie in movies:
         if str(movie["id"]) == str(movieid):
+            # removing the movie
             movies.remove(movie)
-            return make_response(jsonify(movie),200)
+            # deletion is complete
+            return make_response(jsonify(movie), 200)
 
-    res = make_response(jsonify({"error":"movie ID not found"}),400)
-    return res
-        
+    return make_response(jsonify({"error": "movie ID not found"}), 400)
+
+
 if __name__ == "__main__":
-    #p = sys.argv[1]
-    print("Server running in port %s"%(PORT))
-    app.run(host=HOST, port=PORT,debug=True)
+    # p = sys.argv[1]
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT, debug=True)
