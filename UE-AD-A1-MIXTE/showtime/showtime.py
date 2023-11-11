33828daa6f53e00c68d5fa9bcef924e7747b0879
Jana Zebian <jana.zebian@etu.u-bordeaux.fr>
2023-11-11 20:41:39 +0100

    done with showtime

--- UE-AD-A1-MIXTE/showtime/showtime.py
+++ UE-AD-A1-MIXTE/showtime/showtime.py
@@ -10,6 +10,16 @@ class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
 
+    def GetMovies(self, request, context):
+        for movie in self.db:
+            if movie['date'] == request.date:
+                print("Movie Found!")
+                return showtime_pb2.ShowtimeData(date=movie['date'], movies=movie['movies'])
+        return showtime_pb2.ShowtimeData(date="!not found!", movies="!not found!")
+    def GetTimes(self, request, context):
+        for time in self.db:
+            yield showtime_pb2.ShowtimeData(date=time['date'], movies=time['movies'])
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     showtime_pb2_grpc.add_ShowtimeServicer_to_server(ShowtimeServicer(), server)
