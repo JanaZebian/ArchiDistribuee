e553eef66705c1235033621f741ee77462f8d819
Jana Zebian <jana.zebian@etu.u-bordeaux.fr>
2023-11-21 19:54:27 +0100

    entry points to grpc

--- UE-AD-A1-MIXTE/user/user.py
+++ UE-AD-A1-MIXTE/user/user.py
@@ -8,36 +8,6 @@ import grpc
 import booking_pb2
 import booking_pb2_grpc
 
-
-def get_booking_by_user_id(stub, userid):
-    """
-    USER acting as a client calling distant procedure to get a booking by a user id
-    :param stub: Booking
-    :param userid: string
-    :return: void
-    """
-    booking = stub.GetBookingByUserID(userid)
-    print(booking)
-
-
-def get_bookings(stub):
-    """
-    User acting as a client calling distant method to get all the bookings in the database
-    :param stub: Booking
-    :return: void
-    """
-    bookings = stub.GetBookings(booking_pb2.EmptyData())
-    for booking in bookings:
-        print("UserId: %s " % booking.userid)
-        print("Dates: %s" % booking.dates)
-        print()
-
-
-def add_booking_by_user(stub, userid, date, movieid):
-    new_booking = stub.AddBookingByUser(booking_pb2.NewBookingData(userid = userid, date= date, movieid= movieid))
-    print(new_booking.id)
-
-
 app = Flask(__name__)
 
 PORT = 3004
@@ -45,6 +15,52 @@ HOST = '0.0.0.0'
 
 with open('{}/data/users.json'.format("."), "r") as jsf:
     users = json.load(jsf)["users"]
+
+
+@app.route("/booking/<userid>", methods=['GET'])
+def get_booking_by_user_id(userid):
+    """
+    USER acting as a client calling distant procedure to get a booking by a user id
+    :param userid: string
+    :return: json
+    """
+    with grpc.insecure_channel('localhost:3003') as channel:
+        stub = booking_pb2_grpc.BookingStub(channel)
+        for user in users:
+            if user["id"] == userid:
+                booking = stub.GetBookingByUserID(booking_pb2.Id(id=userid))
+                if booking.userid == userid:
+                    booking_list = [(book.date, [m for m in book.movies]) for book in booking.dates]
+
+                return make_response(jsonify(booking_list), 200)
+        return make_response(jsonify({"error": "Userid not found"}), 400)
+
+    # print(booking)
+
+
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_bookings(userid):
+    """
+    User acting as a client calling distant method to get all the bookings in the database
+    :param userid: string
+    :return: json
+    """
+    with grpc.insecure_channel('localhost:3003') as channel:
+        stub = booking_pb2_grpc.BookingStub(channel)
+        for user in users:
+            if user["id"] == userid:
+                bookings = stub.GetBookings(booking_pb2.EmptyData())
+                # to serialize the list: comprehension lists
+                booking_list = [(element.userid, [(el.date, [mov for mov in el.movies]) for el in element.dates])
+                                for element in bookings]
+
+                return make_response(jsonify(booking_list), 200)
+            return make_response(jsonify({"error": "Userid not found"}), 400)
+
+
+def add_booking_by_user(stub, userid, date, movieid):
+    new_booking = stub.AddBookingByUser(booking_pb2.NewBookingData(userid=userid, date=date, movieid=movieid))
+    print(new_booking.id)
 
 
 @app.route("/users/all", methods=['GET'])
@@ -146,19 +162,5 @@ def get_movie_by_its_id(userid, id: str):
 
 
 if __name__ == "__main__":
-    with grpc.insecure_channel('localhost:3003') as channel:
-        stub = booking_pb2_grpc.BookingStub(channel)
-
-        print("-------------- GetBookingByUserID --------------")
-        userid = booking_pb2.Id(id="dwight_schrute")
-        get_booking_by_user_id(stub, userid)
-
-        print("-------------- GetBookings --------------")
-        get_bookings(stub)
-        print("-------------- AddbookingByUser --------------")
-        # add_booking_by_user(stub,userid="dwight_schrute", date="20151130", movieid="720d006c-3a57-4b6a-b18f-9b713b073f3c")
-        print()
-
-    channel.close()
     print("Server running in port %s" % (PORT))
     app.run(host=HOST, port=PORT)
